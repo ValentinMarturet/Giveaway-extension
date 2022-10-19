16e79760ea71e108f24256443ec092086ecca3ad
unknown <valen.martu2206@gmail.com>
2022-10-18 21:55:58 -0300

    Cambiando axios por fetch

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 async function getFilterGiveaways() {
   const options = {
     method: "GET",
-    url: "https://gamerpower.p.rapidapi.com/api/filter",
-    params: { platform: "epic-games-store.steam.android", type: "game.loot" },
     headers: {
       "X-RapidAPI-Key": "03990fb333msha59b5abde0dd4c3p1aa2bfjsne33a15c544d1",
       "X-RapidAPI-Host": "gamerpower.p.rapidapi.com",
     },
   };
 
-  const res = await axios.request(options);
-  document.getElementById("ga-list").innerHTML = res.data
+  const res = await fetch(
+    "https://gamerpower.p.rapidapi.com/api/filter?platform=epic-games-store.steam.android&type=game.loot",
+    options
+  );
+  const result = await res.json();
+  document.getElementById("ga-list").innerHTML = result
     .map(createCard)
     .join("");
-  console.log(res);
+  console.log(result);
 }
 
 function createCard(item) {
